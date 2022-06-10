361f11768d044f7791c058537118cc7ffd4773f2
VanThang <vanthang23032001@gmail.com>
2022-06-10 11:08:12 +0700

    Update Source

--- BIGSCHOOL_LEVANTHANG/Controllers/HomeController.cs
+++ BIGSCHOOL_LEVANTHANG/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
-﻿using System;
+﻿using BIGSCHOOL_LEVANTHANG.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace BIGSCHOOL_LEVANTHANG.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Courses.Include(c => c.Lecturer).Include(c => c.Category).Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
         }
 
         public ActionResult About()
